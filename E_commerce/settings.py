12d8c8a7afc13cb40db195db7c92b69c76097adc
omarkhalil117 <omarkhalil117@gmail.com>
2024-04-01 14:17:05 +0200

    add requirements.txt file

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users'
+    'rest_framewoek.authtoken',
+    'users',
     'order'
 ]
 
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from .views import UserView
 from django.urls import path
 urlpatterns = [
-    path('', UserView.as_view())
+    path('', UserView.as_view()),
+    # path('login', UserLogin.as_view()),
+    # path('register', UserRegister.as_view()),
 ]
