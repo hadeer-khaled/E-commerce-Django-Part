f85619268753224daf26b094a141b9f8d79ee086
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-04-01 12:38:39 +0200

    order , order-item,shipment model without user

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'order'
+    'order',
+    'order_item',
+    'shipment'
 ]
 
 MIDDLEWARE = [
--- order_item/models.py
+++ order_item/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from order.models import Order
 
-# Create your models here.
+class Order_Item(models.Model):
+    order_product_id = models.AutoField(primary_key=True , default=0)
+    product_id = models.IntegerField()
+    # product_id = models.ForeignKey(Product, on_delete=models.CASCADE ,  db_column ='product_id')
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE ,  db_column ='order_id') 
+    quantity = models.IntegerField(default=0)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["order_product_id", "product_id"], name="order_item_composite_key")
+        ]
+
+  
