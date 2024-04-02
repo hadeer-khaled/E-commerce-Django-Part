2a948c2b6e7986cd8967d219a9e02773a232584c
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-04-02 22:07:01 +0200

    delete user_img from user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -4,5 +4,5 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['user_id', 'first_name', 'last_name', 'username', 'image','email', 'phone', 'user_img']
+        fields = ['user_id', 'first_name', 'last_name', 'username', 'image','email', 'phone']
 
