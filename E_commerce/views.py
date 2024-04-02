dfa4c4a4bb1de0da6beac7f5628e29ba7e5830f6
hussein-elmlah <hussein.elmlah7@gmail.com>
2024-04-02 14:12:48 +0200

    add dummy data for products

--- E_commerce/views.py
+++ E_commerce/views.py
@@ -1,9 +1,6 @@
-# products/views.py
-
 from django.http import HttpResponse
 from utils.insert_dummy_data import insert_dummy_data
 
 def insert_dummy_data_view(request):
-    # Call the insert_dummy_data function
     insert_dummy_data()
     return HttpResponse("Dummy data inserted successfully!")
