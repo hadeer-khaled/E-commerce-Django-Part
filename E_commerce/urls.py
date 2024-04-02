01a9e4ae3bc1b914573f6b4c1cd38015d68938a8
hussein-elmlah <hussein.elmlah7@gmail.com>
2024-04-02 16:31:44 +0200

    add product-images GET implementation

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path ,include
 from .views import insert_dummy_data_view
 from categories.views import CategoryListView
+from products.views import ProductListView, ProductImagesView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +10,7 @@ urlpatterns = [
     path('wishlist/',include('wishlist.urls')),
     path('insert-dummy-data/', insert_dummy_data_view, name='insert_dummy_data'),
     path('categories/', CategoryListView.as_view(), name='category-list'),
+    path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/<int:product_id>/images/', ProductImagesView.as_view(), name='product-images'),
     # path('order/',include("order.urls"))
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product, ProductImage
+from .serializers import ProductSerializer
+from utils.query_params import handle_query_params
 
-# Create your views here.
+class ProductListView(APIView):
+    def get(self, request):
+        queryset = Product.objects.all()
+        params = request.query_params
+        search_fields = ['name']
+        
+        try:
+            result = handle_query_params(queryset, params, search_fields)
+            serializer = ProductSerializer(result['data'], many=True)
+            response_data = {
+                'products': serializer.data,
+                'current_page': result['current_page'],
+                'total_pages': result['total_pages'],
+                'total_count': result['total_count']
+            }
+            return Response(response_data)
+        except ValidationError as e:
+            return Response({'error': str(e)}, status=400)
+class ProductImagesView(APIView):
+    def get(self, request, product_id):
+        product_images = ProductImage.objects.filter(product_id=product_id)
+        print(product_images)
+        image_urls = [image.image.url for image in product_images]
+        return Response(image_urls)
