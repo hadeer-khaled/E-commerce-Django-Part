59d96fc463d2feabdd7b0b190df502ce52af1c62
hussein-elmlah <hussein.elmlah7@gmail.com>
2024-04-02 15:11:09 +0200

    Implemented Category GET functionality with handle_query_params utility to retrieve, paginate, filter, sort and search for categories.

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
+from utils.query_params import handle_query_params
 
-# Create your views here.
+class CategoryListView(APIView):
+    def get(self, request):
+        queryset = Category.objects.all()
+        params = request.query_params
+        search_fields = ['name']
+        
+        try:
+            result = handle_query_params(queryset, params, search_fields)
+            serializer = CategorySerializer(result['data'], many=True)
+            return Response(serializer.data)
+        except ValidationError as e:
+            return Response({'error': str(e)}, status=400)
