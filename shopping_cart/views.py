117fe87bf04ded4a000a9f65a665071214f528f2
Nooran Tarek <noort.mohamed@gmail.com>
2024-04-04 13:03:11 +0200

    adding view of delete shopping cart for specific user

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from .models import ShoppingCart
+from .serializer import ShoppingCartSerializer
 
-# Create your views here.
+class DeleteShoppingCartView(APIView):
+    def delete(self, request):
+        user_id = request.data.get('user_id')
+        shopping_cart = get_object_or_404(ShoppingCart, user_id=user_id)
+        shopping_cart.delete()
+
+        return Response({'message': 'Shopping cart deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
