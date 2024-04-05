1beec0ca9c9fbaa9a8c9573a2f200bf71f7dd3c0
hussein-elmlah <hussein.elmlah7@gmail.com>
2024-04-05 17:15:29 +0200

    Updated response of product views to include array of images and category name

--- products/views.py
+++ products/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Product, ProductImage
-from .serializers import ProductSerializer
+from .models import Product, ProductImage, Category
 from utils.query_params import handle_query_params
 from django.core.exceptions import ValidationError
 
@@ -13,9 +12,13 @@ class ProductListView(APIView):
         
         try:
             result = handle_query_params(queryset, params, search_fields)
-            serializer = ProductSerializer(result['data'], many=True)
+            products_data = []
+            for product in result['data']:
+                product_data = get_product_details(product.product_id)
+                products_data.append(product_data)
+                
             response_data = {
-                'products': serializer.data,
+                'products': products_data,
                 'current_page': result['current_page'],
                 'total_pages': result['total_pages'],
                 'total_count': result['total_count']
@@ -27,15 +30,42 @@ class ProductListView(APIView):
 class ProductDetailsView(APIView):
     def get(self, request, product_id):
         try:
-            product = Product.objects.get(pk=product_id)
-            serializer = ProductSerializer(product)
-            return Response(serializer.data)
-        except Product.DoesNotExist:
-            return Response({"error": "Product not found"}, status=404)
+            product_data = get_product_details(product_id)
+            return Response(product_data)
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=404)
         
 class ProductImagesView(APIView):
     def get(self, request, product_id):
-        product_images = ProductImage.objects.filter(product_id=product_id)
-        print(product_images)
-        image_urls = [image.image.url for image in product_images]
+        image_urls = get_image_urls(product_id)
         return Response(image_urls)
+
+def get_image_urls(product_id):
+    product_images = ProductImage.objects.filter(product_id=product_id)
+    image_urls = [image.image.url for image in product_images]
+    return image_urls
+
+def get_category(category_id):
+    category = Category.objects.filter(category_id=category_id)
+    category_name = category[0].name
+    return category_name
+
+def get_product_details(product_id):
+    try:
+        product = Product.objects.get(pk=product_id)
+        image_urls = get_image_urls(product_id)
+        category_name = get_category(product.category.category_id)
+      
+        product_data = {
+            "product_id": product.product_id,
+            "name": product.name,
+            "price": product.price,
+            "stock": product.stock,
+            "description": product.description,
+            "avg_rating": product.avg_rating,
+            "category": category_name,
+            "images": image_urls
+        }
+        return product_data
+    except Product.DoesNotExist:
+        raise ValidationError("Product not found")
