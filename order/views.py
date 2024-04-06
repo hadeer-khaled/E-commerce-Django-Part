5a09670c9b2e03016a7d15038bba79528a864890
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-04-07 01:02:13 +0200

    patch shipment status api

--- order/views.py
+++ order/views.py
@@ -3,6 +3,7 @@ from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Order
+from shipment.models import Shipment
 from .serializers import OrderSerializer
 
 class AllOrdersView(APIView):
@@ -24,6 +25,20 @@ class SpecificOrderView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Order.DoesNotExist:
             return Response({"error": "Order does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        
+    def patch(self, request, order_id):
+        try:
+            order = Order.objects.get(order_id=order_id)
+            shipment = order.shipment_id
+            # new_status = request.query_params.get('shipmentStatus')
+            shipment.status = "cancelled"
+            shipment.save()
+            return Response("Shipment status updated successfully.", status=status.HTTP_200_OK)
+        except Order.DoesNotExist:
+            return  Response({"error": "Order does not exist."}, status=status.HTTP_404_NOT_FOUND)
+        except shipment.DoesNotExist:
+            return Response({"error": "Shipment does not exist for this order."}, status=status.HTTP_404_NOT_FOUND)
+
         
 class SpecificUserOrdersView(APIView):
     permission_classes = (permissions.AllowAny,)
