3024990937b17c6541b0eacadb553dc9a137b0f4
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-04-07 01:02:13 +0200

    patch shipment status api

--- shipment/models.py
+++ shipment/models.py
@@ -4,12 +4,14 @@ class Shipment(models.Model):
     stauts_choices = [ ("pending","pending") , ("shipped" ,"shipped") ,("delivered" ,"delivered") , ("cancelled" , "cancelled")]
 
     shipment_id =models.AutoField(primary_key=True , default=1)
-    # user_id = models.IntegerField()
-    # user_id = models.ForeignKey(User, on_delete=models.CASCADE ,  db_column ='user_id') 
-    # order_id = models.ForeignKey(Order, on_delete=models.CASCADE ,  db_column ='order_id') 
     status = models.CharField(max_length = 10,choices = stauts_choices , default = "pending" )
     delivery_date = models.DateTimeField()
 
     # class Meta:
     #     constraints = [
     #         models.UniqueConstraint(fields=["user_id", "order_id"], name="shipment_composite_key")]
+
+
+# user_id = models.IntegerField()
+# user_id = models.ForeignKey(User, on_delete=models.CASCADE ,  db_column ='user_id') 
+# order_id = models.ForeignKey(Order, on_delete=models.CASCADE ,  db_column ='order_id') 
